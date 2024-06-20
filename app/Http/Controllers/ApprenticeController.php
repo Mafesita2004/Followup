c15882a71f7c482c563e0214a8e4325d7c7a66e3
unknown <astridcantero2005@gmail.com>
2024-06-20 17:50:34 -0500

    subir tablas de roles

---
+++ app/Http/Controllers/ApprenticeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\apprentice;
+use Illuminate\Http\Request;
+
+class ApprenticeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(apprentice $apprentice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(apprentice $apprentice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, apprentice $apprentice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(apprentice $apprentice)
+    {
+        //
+    }
+}
