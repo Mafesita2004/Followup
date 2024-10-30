2e8a4102b3f5c46def904a2f75e92aadd7e08ed5
LauraOrozco1320 <orozcolauracamila@gmail.com>
2024-10-30 10:53:04 -0500

    INSTRUCTOR

--- resources/views/trainer/visita.blade.php
+++ resources/views/trainer/visita.blade.php
@@ -53,30 +53,19 @@
                     </button>
                 </div>
     </header>
-    <nav class="bg-[#009e00] px-2.5 h-14 py-1.5 flex justify-start items-center relative z-10">
-        <button id="notifButton" class="relative">
-            <img class="w-[35px] h-auto mr-2.5 filter invert" src="{{ asset('img/notificaciones.png') }}" alt="Notificaciones">
-            <span class="absolute top-0 right-0 w-4 h-4 bg-red-600 text-white text-xs rounded-full flex items-center justify-center">5</span> <!-- Ejemplo de contador de notificaciones -->
-        </button>
-        <div id="notifMenu" class="hidden absolute top-full mt-2 left-0 w-64 bg-white border border-gray-300 rounded-lg shadow-lg z-20">
-            <div class="p-4">
-                <h2 class="text-sm font-bold">Notificaciones</h2>
-                <ul>
-                    <li class="mt-2">
-                        <a href="{{ route('notification') }}" class="block text-gray-700 hover:bg-gray-100 p-2 rounded-lg">Notificación 1</a>
-                    </li>
-                    <li class="mt-2">
-                        <a href="{{ route('notification') }}" class="block text-gray-700 hover:bg-gray-100 p-2 rounded-lg">Notificación 2</a>
-                    </li>
-                    <li class="mt-2">
-                        <a href="{{ route('notification') }}" class="block text-gray-700 hover:bg-gray-100 p-2 rounded-lg">Notificación 3</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="text-white text-center absolute left-1/2 transform -translate-x-1/2">Concenrtación</div>
-        <div class="relative ml-auto flex items-center ">
+     <!--Notificaciones -->
+    <nav class="bg-[#009e00] px-2.5 h-14 py-1.5 flex items-center relative z-10">
+        <div class="text-white text-center absolute left-1/2 transform -translate-x-1/2">Concentración</div>
 
+        <div class="relative ml-auto flex items-center">
+            <button id="notifButton" class="relative">
+                <a href="{{ route('notification') }}">
+                <img class="w-[50px] h-auto mr-3.0 filter invert" src="{{ asset('img/notificaciones.png') }}" alt="Notificaciones">
+
+            </button>
+
+
+        <div class="relative ml-auto flex items-center ">
 
             <div id="userMenuTri" class=" hidden absolute right-4  mt-2 w-64 bg-[#D9D9D9] border border-gray-300 rounded-lg shadow-lg z-20">
                 <div class="p-4">
@@ -89,7 +78,7 @@
                         <img src="{{ asset('img/administrador/mujer.png') }}" alt="User Icon" class="w-10 h-10 rounded-full mr-3 mx-10 bg-white border-black border-2">
                     </div>
                     <ul>
-                        <a href="{{ route('username')}}" class="block text-center text-green-600 font-bold mt-4 bg-white border hover:text-white hover:bg-green-600 border-green-600 rounded-lg py-1">ver perfil</a>
+                        <a href="{{ route('username')}}" class="block text-center text-green-600 font-bold mt-4 bg-white border hover:text-white hover:bg-green-600 border-green-600 rounded-lg py-1">Ver perfil</a>
                         <li class="mt-2"><a href="{{ route('configuracion')}}" class="block text-black hover:bg-white p-2 rounded-lg">Configuración</a></li>
                         <li class="mt-2"><a href="{{ route('icon') }}" class="block text-black hover:bg-white p-2 rounded-lg">Aprendices</a></li>
                         <li class="mt-2"><a href="{{ route('cronograma') }}" class="block text-black hover:bg-white p-2 rounded-lg">Cronograma</a></li>
@@ -137,12 +126,12 @@
             <div class="w-full flex space-x-4 items-center justify-between text-center">
                 <div class="flex flex-col">
                     <label class="font-bold">N° Visita</label>
-                    <select class="border border-gray-400 p-2 rounded-md w-48 bg-white text-center">
+                    <select class="border border-gray-400 p-2 rounded-md w-48 bg-white">
                         <option selected="">Selecciona Opción</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
+                        <option value="Inicial">Inicial</option>
+                        <option value="Parcial">Parcial</option>
+                        <option value="Final">Final</option>
+                        <option value="Mejoramiento">Mejoramiento</option>
                     </select>
                 </div>
                 <div class="flex flex-col">
